ab9079a5ecb0cbea3016934dbe951088f10f8911
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2026-04-10 11:45:22 +0700

    Kode ini berfungsi sebagai konfigurasi awal (inisialisasi) saat aplikasi pertama kali dijalankan untuk memastikan tampilan form dan tabel siap digunakan.

--- Form1.cs
+++ Form1.cs
@@ -270,6 +270,22 @@ namespace CRUDMahasiswaADO
             txtNIM.Focus();
         }
 
+        //langkah 12 - mengatur tampilan awal form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
